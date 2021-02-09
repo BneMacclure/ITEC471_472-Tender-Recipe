f1b12fe86245752423823777c2c3db0b9dda3aca
Benjamin McClure <bmcclure3@radford.edu>
2021-02-08 20:50:10 -0500

    Login is now implemented. Firebase connection setup

---
+++ config/DatabaseConfig.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDeCWlbM7NNorXrl1oOnjVSwwS2n8hcq9s",
+    authDomain: "tenderrecipe.firebaseapp.com",
+    databaseURL: "https://tenderrecipe-default-rtdb.firebaseio.com",
+    projectId: "tenderrecipe",
+    storageBucket: "tenderrecipe.appspot.com",
+    messagingSenderId: "676253799900",
+    appId: "1:676253799900:web:10622e66267c76f5e22b19"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.database();
+
+export {firebaseApp, db}
