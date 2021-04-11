c2d6b2fda20a3311ae6fc20756879dd3bc818a68
BucaroMarissa <bucaromarissa@gmail.com>
2021-04-10 20:48:07 -0400

    added minus button to delete individual ingredients

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -286,6 +286,7 @@ export default class LoginScreen extends React.Component {
                                                 placeholderTextColor="rgba(150,150,150,1)"
                                                 inlineImagePadding={0}
                                                 selectionColor="rgba(52,250,215,1)"
+                                                autoCapitalize="none"
                                                 style={styles.textInput}
                                                 onChangeText={(email) => this.setState({ email: email })}
                                             ></TextInput>
